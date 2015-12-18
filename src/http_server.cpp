b54aa800a6534ce5be72c45fb49b7e9d883e5b8a
chudov <roman.chudov@sibers.com>
2015-12-18 10:30:27 +0600

    small fix

--- src/http_server.cpp
+++ src/http_server.cpp
@@ -189,7 +189,7 @@ namespace Network {
     //----------------------------------------------------------------------
     void HttpServer::stop() {
         m_isRun = false;
-        //std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 
     void HttpServer::wait() {
